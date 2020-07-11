fc80fa4c0d897820f6723ca0b81333c62d2589c2
Shrenik <30378730+shrenik94@users.noreply.github.com>
2020-07-11 13:14:52 -0700

    added react-router

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                hello from singular room page
+            </div>
+        )
+    }
+}
+
