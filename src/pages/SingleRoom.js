e94c4a0665e94c25e36f4939d6bd45578c81d202
Shrenik <30378730+shrenik94@users.noreply.github.com>
2020-07-12 15:27:01 -0700

    completed the home page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from '../context';
 
 export default class SingleRoom extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        }
+    }
+    static contextType = RoomContext;
+
+    componentDidMount() {
+
+    }
+
     render() {
+        const {getRoom} = this.context
+        const room = getRoom(this.state.slug);
+        if(!room) {
+            return <div className="error">
+                <h3>No such room could be found</h3>
+                <Link to='/rooms' className='btn-primary'>
+                    Back to rooms
+                </Link>
+            </div>
+        }
         return (
             <div>
-                hello from singular room page
+                hello from singular room page{room.name}
             </div>
         )
     }
